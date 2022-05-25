613a2000a058732fa488e1f8cd880239882a836a
lh99j <grand2181@gmail.com>
2022-05-25 17:26:04 +0900

    Jubging

---
+++ Jubging/app/src/main/java/com/example/jubging/JsonPasingView.java
@@ -0,0 +1,16 @@
+package com.example.jubging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JsonPasingView extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json_pasing_view);
+    }
+}
