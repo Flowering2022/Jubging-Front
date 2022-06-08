b702fffc1d40ddedf2b97a878a7ec36e56b199e1
lh99j <grand2181@gmail.com>
2022-06-08 16:23:21 +0900

    운동기록 추가

---
+++ Jubging/app/src/main/java/com/example/jubging/DataClass.java
@@ -0,0 +1,28 @@
+package com.example.jubging;
+
+import com.google.gson.annotations.SerializedName;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public class DataClass {
+    // @SerializedName으로 일치시켜 주지않을 경우엔 클래스 변수명이 일치해야함
+    @SerializedName("distance__sum")
+    public int distance__sum;
+
+    @SerializedName("plogging_freq")
+    public int plogging_freq;
+
+
+
+    // toString()을 Override 해주지 않으면 객체 주소값을 출력함
+    @Override
+    public String toString() {
+        return "PostResult{" +
+                "distance__sum=" + distance__sum +
+                "plogging_freq" + plogging_freq +
+                '\'' +
+                '}';
+    }
+}
--- Jubging/app/src/main/java/com/example/jubging/MainActivity.java
+++ Jubging/app/src/main/java/com/example/jubging/MainActivity.java
@@ -21,7 +21,9 @@ import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -51,6 +53,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.MapViewEventListener, MapView.POIItemEventListener
 {
@@ -99,8 +108,6 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
         final ImageButton btn = (ImageButton) findViewById(R.id.trc);
         btn.bringToFront();
 
-
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -114,8 +121,163 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
             }
         });
 
-        final ImageButton plogingst = (ImageButton)findViewById(R.id.btn_pause);
-        plogingst.bringToFront();
+        //서버 연결헤서 데이터 가져오기
+        //Retrofit 인스턴스 생성
+        retrofit2.Retrofit retrofit = new retrofit2.Retrofit.Builder()
+                .baseUrl("http://ec2-52-79-240-128.ap-northeast-2.compute.amazonaws.com/") //baseUrl
+                .addConverterFactory(GsonConverterFactory.create()) //JSON을 변환해줄 Gson 변환기 등록
+                .build();
+
+        RetrofitInterface service = retrofit.create(RetrofitInterface.class); //RetrofitInterface 겍체 구현
+
+        final Button btn_finish = findViewById(R.id.btn_finish); //전송 버튼
+
+        final ImageButton btn_pause = findViewById(R.id.btn_pause);
+        btn_pause.bringToFront();
+
+        btn_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_pause.setVisibility(View.INVISIBLE); //시작버튼 누르면 시작버튼 없애기
+                btn_finish.setVisibility(View.VISIBLE); //운동완료 버튼 보이기
+                btn_finish.setEnabled(true); //운동완료버튼 활성화
+                btn_finish.setText("플로깅 완료하기"); //버튼 내 택스트 변경
+            }
+        });
+
+        btn_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_finish.setText("오늘도 지구를 지키셨습니다!"); //버튼 내 택스트 변경
+                btn_pause.setVisibility(View.VISIBLE); //시작 버튼 보이기
+                btn_finish.setEnabled(false); //운동완료버튼 비활성화
+
+                //서버 주소/1 전달
+                Call<DataClass> call = service.getName("1");
+
+                call.enqueue(new Callback<DataClass>(){
+                    @Override
+                    public void onResponse(Call<DataClass> call, Response<DataClass> response){
+                        if(response.isSuccessful()){
+                            DataClass result = response.body();
+
+                            //서버에서 응답방은 데이터를 TextView에 넣어준다.
+                            final TextView distance__sum = findViewById(R.id.distance);
+                            final TextView plogging_freq = findViewById(R.id.number);
+
+                            distance__sum.setText(result.distance__sum+"");
+                            plogging_freq.setText(result.plogging_freq+"");
+
+                        }
+                        else{
+                            //실패
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<DataClass> call, Throwable t){
+                        //통신 실패
+                    }
+                });
+            }
+        });
+//        //Post.. url을 못가져와서 인가..
+//        Call<List<JsonPlaceHOlderApi.Post>> call = jsonPlaceHOlderApi.getPosts();
+//
+//        call.enqueue(new Callback<List<JsonPlaceHOlderApi.Post>>() {
+//            @Override
+//            public void onResponse(Call<List<JsonPlaceHOlderApi.Post>> call, Response<List<JsonPlaceHOlderApi.Post>> response) {
+//                if (!response.isSuccessful())
+//                {
+//                    distance.setText("Code: " + response.code());
+//                    return;
+//                }
+//
+//                List<JsonPlaceHOlderApi.Post> posts = response.body();
+//
+//                for (JsonPlaceHOlderApi.Post post : posts) {
+//                    String content ="";
+//                    content += "Distance : " + post.getDistance__sum() + "\n";
+//
+//                    distance.append(content);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<JsonPlaceHOlderApi.Post>> call, Throwable t) {
+//                distance.setText(t.getMessage());
+//            }
+//        });
+////////////////////////////////////////////////////////////////////////////////
+//        //화면 전환 버튼 동작
+//        //Button btn_finish = (Button) findViewById(R.id.btn_finish);
+//        btn_finish = findViewById(R.id.btn_finish);
+//        btn_finish.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent(getApplicationContext(), CompleteActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+/////////////////////////////////////////////////////////////////////////////////
+//        //플로깅 총 횟수
+//        number = findViewById(R.id.number);
+//        number.setText(count+"");
+//        btn_finish = findViewById(R.id.btn_finish);
+//        btn_finish.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                while (count >= 0) {
+////                    count++;
+////                    btn_count.setText(count + "");
+////                }
+//                @Override
+//                public void onClick(View v) {
+//                    if (count == 0){
+//                        count++;
+//                    }
+//                    else {
+//                        number.setText(count + "");
+//                        count++;
+//                    }
+//
+////                if (count %2 == 1) {
+////                    btn_count.setText(count + "");//1 //3
+////                    count++;//2 //4
+////                }
+//
+////               if (count ==0){
+////                    count++; //1
+////                } else if (count == 1) {
+////                    btn_count.setText(count + ""); //1
+////                    count++; //2
+////                }
+////                else if(count==2){
+////                    count++; //3
+////                }
+////                else if (count == 3) {
+////                    count-=1; //2
+////                    btn_count.setText(count + ""); //2
+////                    count += 2; //4
+////                }
+////                else if (count ==4){
+////                    count++; //5
+////                }
+////                else if (count ==5){
+////                   count-=2; //3
+////                   btn_count.setText(count + ""); //3
+////                   count += 3; //6
+////               }
+////               else if (count ==6){
+////                   count++; //7
+////               }
+////               else if (count ==7){
+////                   count-=3; //4
+////                   btn_count.setText(count + ""); //4
+////                   count += 4; //8
+////               }
+//
+//            }
+//        });
 
 
         final ImageButton button = (ImageButton) findViewById(R.id.location_Btn);
